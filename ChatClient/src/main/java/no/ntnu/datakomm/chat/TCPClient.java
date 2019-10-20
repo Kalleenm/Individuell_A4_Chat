26760fca577468033132b14f3c22ec3ab72f827c
Karl-Oskar Norheim Molvær <komolvae@stud.ntnu.no>
2019-10-21 01:53:21 +0200

    Bug fix, command not supported in sentCommand

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -90,49 +90,39 @@ public class TCPClient {
      * @param cmd A command. It should include the command word and optional attributes, according to the protocol.
      * @return true on success, false otherwise
      */
-    private boolean sendCommand(String cmd)
-    {
+    private boolean sendCommand(String cmd) {
         // TODO Step 2: Implement this method
-        // Hint: Remember to check if connection is active
         boolean attempt = false;
 
         // Check if the connection is active.
-        if (isConnectionActive())
-        {
-            if(cmd != null)
-            {
+        if (isConnectionActive()) {
+            if (cmd != null) {
                 String editedCutCmdCommand = null;
-                String[] parts = cmd.split("");
+                String[] parts = cmd.split(" ");
                 String cmdCommand = parts[0];
-                if(cmdCommand.endsWith("/"))
-                {
+                if (cmdCommand.startsWith("/")) {
                     String cutCmdCommand = cmdCommand.replace("/", "");
-                            if(cutCmdCommand.endsWith("\n"))
-                            {
-                                editedCutCmdCommand = cutCmdCommand.replace("\n", "");
-                            }
-                            else
-                            {
-                                editedCutCmdCommand = cutCmdCommand;
-                            }
-                            if( editedCutCmdCommand.equals("privmsg")||
-                                editedCutCmdCommand.equals("help")   ||
-                                editedCutCmdCommand.equals("login")  ||
-                                editedCutCmdCommand.equals("user"))
-                            {
-                                String editedCmd = cmd.substring(1, cmd.length());
+                    if (cutCmdCommand.endsWith("\n")) {
+                        editedCutCmdCommand = cutCmdCommand.replace("\n", "");
+                    } else {
+                        editedCutCmdCommand = cutCmdCommand;
+                    }
+                    if (editedCutCmdCommand.equals("privmsg") ||
+                            editedCutCmdCommand.equals("help") ||
+                            editedCutCmdCommand.equals("login") ||
+                            editedCutCmdCommand.equals("user")) {
+                        String editedCmd = cmd.substring(1, cmd.length());
 
-                                // Print out to the console for debugging purposes.
-                                System.out.println("Sending command: " + editedCmd);
-                                attempt = true;
-                                toServer.println(editedCmd);
-                            }
-                }
-                else
-                {
+                        // Print out to the console for debugging purposes.
+                        System.out.println("Sending command: " + editedCmd);
+                        this.toServer.println(editedCmd);
+                        attempt = true;
+
+                    }
+                } else {
                     System.out.println("Sending message: " + cmd);
+                    this.toServer.println(cmd);
                     attempt = true;
-                    toServer.println("msg" + cmd);
                 }
             }
         }
@@ -202,7 +192,7 @@ public class TCPClient {
         // Hint: Reuse sendCommand() method
         // Hint: update lastError if you want to store the reason for the error.
         boolean result = false;
-        result = sendCommand("privatemsg" + recipient + " " + message);
+        result = sendCommand("privmsg" + recipient + " " + message);
         return result;
     }
 
